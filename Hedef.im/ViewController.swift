1345f31ea159c0e944e6881daeea6c0d17b5159d
aybarscengaver <aybarscengaver@gmail.com>
2015-05-01 12:28:53 +0300

    Initial Commit

---
+++ Hedef.im/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hedef.im
+//
+//  Created by Emre YILMAZ on 1.05.2015.
+//  Copyright (c) 2015 AybarsCengaver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
